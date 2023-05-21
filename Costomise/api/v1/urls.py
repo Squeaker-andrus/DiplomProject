bfe709cf595a4562e0ee3201a78ac7657bf0fb6a
Squeaker-andrus <andrussvarog1024@gmail.com>
2023-05-22 01:08:42 +0300

    V0.3

--- Costomise/api/v1/urls.py
+++ Costomise/api/v1/urls.py
@@ -5,7 +5,7 @@ from . import views
 # Costomise api v1
 
 urlpatterns = [
-    path("settings/", views.ProfileUpdateView.as_view(), name="update_profile"),
+    path("settings/", views.update_profile, name="update_profile"),
     path("", views.player_info, name="show_profile"),
     # path("logout/", views.logout_user, name="logout")
 ]
--- Costomise/api/v1/views.py
+++ Costomise/api/v1/views.py
@@ -5,6 +5,7 @@ from django.views.generic import UpdateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.http import HttpResponse, HttpResponseForbidden
+from django.contrib.auth.models import User
 
 
 def player_info(request):
@@ -12,17 +13,56 @@ def player_info(request):
     return render(request, "second_pattern/user_profile.html", {"player_info": all_info})
 
 
-class ProfileUpdateView(LoginRequiredMixin, UpdateView):
-    model = Player
-    form_class = UserInfo
-    template_name = 'second_pattern/user_profile_settings.html'
-    success_url = reverse_lazy('show_profile')
+# class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+#     model = Player
+#     form_class = UserInfo
+#     template_name = 'second_pattern/user_profile_settings.html'
+#     success_url = reverse_lazy('show_profile')
+#
+#     def get_object(self):
+#         print(self.request.POST)
+#         return self.request.user.player
+#
+#     def form_valid(self, form):
+#         if form.instance.user != self.request.user:
+#             return HttpResponseForbidden()
+#         return super().form_valid(form)
 
-    def get_object(self):
-        print(self.request.POST)
-        return self.request.user.player
 
-    def form_valid(self, form):
-        if form.instance.user != self.request.user:
-            return HttpResponseForbidden()
-        return super().form_valid(form)
+def update_profile(request):
+    print(request.user.player)
+    data = Player.objects.get(id=request.session.get('_auth_user_id'))
+    print(request.POST)
+    print(data)
+    if request.method == "POST":
+        print("check1....................")
+        first_name = request.POST.get("first_name", "")
+        last_name = request.POST.get("last_name", "")
+        user_pick = request.FILES.get("user_pick")
+        country = request.POST.get("country", "")
+        age = request.POST.get("age", "")
+        email = request.POST.get("email", "")
+        about = request.POST.get("about", "")
+        print("check2.........................")
+        print(first_name, last_name, country, age, email, about)
+        if first_name and last_name and user_pick and country and age and email and about:
+            data.first_name = first_name
+            data.last_name = last_name
+            data.user_pick = user_pick
+            data.country = country
+            data.age = age
+            data.email = email
+            data.about = about
+            data.save()
+            print('check3..................')
+            return redirect("show_profile")
+    print("check4.....................")
+    return render(request, "second_pattern/user_profile_settings.html", {"first_name": data.first_name,
+                                                                "last_name": data.last_name,
+                                                                "user_pick": data.user_pick,
+                                                                "country": data.country,
+                                                                "age": data.age,
+                                                                "email": data.email,
+                                                                "about": data.about
+                                                                }
+                  )
